8a7a6cef1a9ca441bf8ed8fb6fe008ab662da071
ayushjain27 <ayushjain201574@gmail.com>
2022-09-12 17:49:52 +0530

    Updated

--- src/Components/Home/Quotes.jsx
+++ src/Components/Home/Quotes.jsx
@@ -8,7 +8,7 @@ const Quotes = () => {
         <div className={`${styles.containerFluid} container-fluid p-3`}>
             <div className={styles.box}>
               <span className={styles.span}></span>
-              <p className={styles.text}>Ayush</p>
+              <p className={`${styles.text} fs-4 fw-bold text-center`}>Life gives you 100 reasons to smile. Be a reason for someone</p>
             </div>
         </div>
         <div className={`${styles.JoinContainer} d-flex justify-content-center align-items-center w-100`}>
