ea93e04216e4e337c6d53944ce453882069febe9
ayushjain27 <ayushjain201574@gmail.com>
2022-09-14 17:40:30 +0530

    Updated

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -7,12 +7,12 @@ const Items = () => {
       <div className="container text-center">
           <div className="mt-2 mb-2 fs-2 fw-bold">Your Items</div>
         <div className="row">
-          <div className="col-md-6">One of three columns</div>
-          <div className="col-md-6">One of three columns</div>
+          <div className="col-md-6">Footwear</div>
+          <div className="col-md-6">Clothes</div>
         </div>
         <div className="row">
-          <div className="col-md-6">One of three columns</div>
-          <div className="col-md-6">One of three columns</div>
+          <div className="col-md-6">Books</div>
+          <div className="col-md-6">Money</div>
         </div>
       </div>
     </>
