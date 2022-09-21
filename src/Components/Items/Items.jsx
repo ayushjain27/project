0d0a7b27465b7f65404f253248386767efffa107
ayushjain27 <ayushjain201574@gmail.com>
2022-09-21 21:08:57 +0530

    Updated

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -7,12 +7,12 @@ const Items = () => {
       <div className="container text-center">
           <div className="mt-2 mb-2 fs-2 fw-bold">Your Items</div>
         <div className="row">
-          <div className="col-md-6 btn btn-warning"><h3>Footwear</h3></div>
-          <div className="col-md-6 btn btn-warning"><h3>Clothes</h3></div>
+          <div className="col-md-6 btn btn-warning p-3"><h3>Footwear</h3></div>
+          <div className="col-md-6 btn btn-warning p-3"><h3>Clothes</h3></div>
         </div>
         <div className="row">
-          <div className="col-md-6 btn btn-warning"><h3>Books</h3></div>
-          <div className="col-md-6 btn btn-warning"><h3>Money Deposit</h3></div>
+          <div className="col-md-6 btn btn-warning p-3"><h3>Books</h3></div>
+          <div className="col-md-6 btn btn-warning p-3"><h3>Money Deposit</h3></div>
         </div>
       </div>
     </>
