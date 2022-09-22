5ece541c79f0bdd2b3fd1e3993056b9222fcc634
ayushjain27 <ayushjain201574@gmail.com>
2022-09-22 16:29:36 +0530

    Updated

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-import styles from "./Items.module.css";
+// import styles from "./Items.module.css";
 
 const Items = () => {
   return (
     <>
       <div className="container text-center">
-          <div className="mt-2 mb-2 fs-2 fw-bold">Your Items</div>
-        <div className="row">
-          <div className="col-md-6 btn btn-warning p-3"><h3>Footwear</h3></div>
-          <div className="col-md-6 btn btn-warning p-3"><h3>Clothes</h3></div>
+        <div className="mt-2 mb-2 fs-2 fw-bold">Your Items</div>
+        <div className="row my-2">
+          <div className="col-md-6 btn btn-warning p-3">
+            <div className="box">
+              <h3>Footwear</h3>
+            </div>
+          </div>
+          <div className="col-md-6 btn btn-warning p-3">
+            <div className="box">
+              <h3>Clothes</h3>
+            </div>
+          </div>
         </div>
-        <div className="row">
-          <div className="col-md-6 btn btn-warning p-3"><h3>Books</h3></div>
-          <div className="col-md-6 btn btn-warning p-3"><h3>Money Deposit</h3></div>
+        <div className="row my-2">
+          <div className="col-md-6 btn btn-warning p-3">
+            <div className="box">
+              <h3>Books</h3>
+            </div>
+          </div>
+          <div className="col-md-6 btn btn-warning p-3">
+            <div className="box">
+              <h3>Money Deposit</h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
