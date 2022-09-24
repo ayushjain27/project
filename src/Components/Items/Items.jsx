6ae483a294d72fb4e031749f0875db08921434a6
ayushjain27 <ayushjain201574@gmail.com>
2022-09-24 14:13:26 +0530

    Updated

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -7,26 +7,26 @@ const Items = () => {
       <div className="container text-center">
         <div className="mt-2 mb-2 fs-2 fw-bold">Your Items</div>
         <div className="row my-2">
-          <div className="col-md-6 btn btn-warning p-3">
-            <div className="box w-100">
-              <h3>Footwear</h3>
+          <div className="col-md-6 p-2">
+            <div className="box w-100  btn btn-warning">
+              <h3 className="p-1">Footwear</h3>
             </div>
           </div>
-          <div className="col-md-6 btn btn-warning p-3">
-            <div className="box w-100">
-              <h3>Clothes</h3>
+          <div className="col-md-6 p-2">
+            <div className="box w-100  btn btn-warning">
+              <h3 className="p-1">Clothes</h3>
             </div>
           </div>
         </div>
         <div className="row my-2">
-          <div className="col-md-6 btn btn-warning p-3">
-            <div className="box w-100">
-              <h3>Books</h3>
+          <div className="col-md-6 p-2">
+            <div className="box w-100  btn btn-warning">
+              <h3 className="p-1">Books</h3>
             </div>
           </div>
-          <div className="col-md-6 btn btn-warning p-3">
-            <div className="box w-100">
-              <h3>Money Deposit</h3>
+          <div className="col-md-6 p-2">
+            <div className="box w-100  btn btn-warning">
+              <h3 className="p-1">Money Deposit</h3>
             </div>
           </div>
         </div>
