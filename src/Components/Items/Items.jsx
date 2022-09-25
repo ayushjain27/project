3bb02af246f89c06dad0ced445f80f6cc8063fd9
ayushjain27 <ayushjain201574@gmail.com>
2022-09-25 12:34:56 +0530

    Updated

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import styles from "./Items.module.css";
+import styles from "./Items.module.css";
 
 const Items = () => {
   return (
@@ -8,24 +8,36 @@ const Items = () => {
         <div className="mt-2 mb-2 fs-2 fw-bold">Your Items</div>
         <div className="row my-2">
           <div className="col-md-6 p-2">
-            <div className="box w-100  btn btn-warning">
+            <div className="box w-100 btn btn-warning">
+              <h3 className="p-1">Footwear</h3>
+            </div>
+            <div className={`${styles.entries} box w-100 btn btn-danger`}>
               <h3 className="p-1">Footwear</h3>
             </div>
           </div>
           <div className="col-md-6 p-2">
-            <div className="box w-100  btn btn-warning">
+            <div className="box w-100 btn btn-warning">
+              <h3 className="p-1">Clothes</h3>
+            </div>
+            <div className={`${styles.entries} box w-100 btn btn-danger`}>
               <h3 className="p-1">Clothes</h3>
             </div>
           </div>
         </div>
         <div className="row my-2">
           <div className="col-md-6 p-2">
-            <div className="box w-100  btn btn-warning">
+            <div className="box w-100 btn btn-warning">
+              <h3 className="p-1">Books</h3>
+            </div>
+            <div className={`${styles.entries} box w-100 btn btn-danger`}>
               <h3 className="p-1">Books</h3>
             </div>
           </div>
           <div className="col-md-6 p-2">
-            <div className="box w-100  btn btn-warning">
+            <div className="box w-100 btn btn-warning">
+              <h3 className="p-1">Money Deposit</h3>
+            </div>
+            <div className={`${styles.entries} box w-100 btn btn-danger`}>
               <h3 className="p-1">Money Deposit</h3>
             </div>
           </div>
