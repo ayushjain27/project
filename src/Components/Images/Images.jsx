8ad76f4b8c09f449fabaa513dbfc81bee342d8bd
ayushjain27 <ayushjain201574@gmail.com>
2022-09-25 16:30:57 +0530

    Updated

--- src/Components/Images/Images.jsx
+++ src/Components/Images/Images.jsx
@@ -122,7 +122,7 @@ const Images = () => {
                 <button
                   onClick={handleSubmit}
                   disabled={
-                    imag.title.length < 4 || imag.description.length < 10 || imag.text !== "Select NGO..."
+                    imag.title.length < 4 || imag.description.length < 10 || imag.text == "Select NGO..."
                   }
                   className={`${styles.button} btn btn-warning my-3`}
                   type="submit"
