0c09214775f2df57caa4b465b4f1e2b9ab0c0ce8
ayushjain27 <ayushjain201574@gmail.com>
2022-10-23 14:23:54 +0530

    Updated

--- Backened/routes/auth.js
+++ Backened/routes/auth.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 // Create a User using: POST "/api/auth". Doesn't tequire auth
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/', [
+    body('name', 'Name must be atleast 5 characters').isLength({ min: 5 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+], (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user=>res.json(user))
+    .catch(err=>{console.log(err)
+    res.json({error: 'Please enter a unique value for email', message: err.message})});
+    // res.send(req.body);
 })
 
 module.exports = router;
