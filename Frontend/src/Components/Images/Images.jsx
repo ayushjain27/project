8b0066426b0cb0c299fb3308249ce8af20012cbc
ayushjain27 <ayushjain201574@gmail.com>
2022-10-27 19:24:49 +0530

    Updated

--- Frontend/src/Components/Images/Images.jsx
+++ Frontend/src/Components/Images/Images.jsx
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Images.module.css";
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+// import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 const Images = () => {
   // const [text, setText] = useState("Select NGO...");
   const [selectedImage, setSelectedImage] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
-  const [imag, setImag] = useState({ title: "", description: "", text: "SELECT NGO..." });
+  const [imag, setImag] = useState({ title: "", description: "", text: "" });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setImag({ title: "", description: "", text: "SELECT NGO..." });
+    setImag({ title: "", description: "", text: "" });
     console.log("clicked");
+    console.log(imag.title);
+    console.log(imag.description);
+    console.log(imag.text);
   };
 
   // const handleOnChange = (event) => {
@@ -71,6 +81,16 @@ const Images = () => {
               <div className="my-3">
                 <div className={`${styles.heading} my-3`}>Your info</div>
                 <div className="my-3">
+                    {/* <Box
+                      component="form"
+                      sx={{
+                        '& > :not(style)': { m: 1, width: '25ch' },
+                      }}
+                      noValidate
+                      autoComplete="off"
+                      >
+                      <TextField id="outlined-basic" label="Enter title" variant="outlined" name="title" value={imag.title} onChange={onChange}/>
+                    </Box> */}
                   <input
                     type="text"
                     id="title"
@@ -87,6 +107,26 @@ const Images = () => {
                     Your title must be atleast 4 characters long
                   </div>
                 </div>
+                  {/* <Box
+                    component="form"
+                    sx={{
+                      '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <div>
+                      <TextField
+                        id="outlined-multiline-flexible"
+                        label="Enter description"
+                        placeholder="Enter description (Size, Quantity, Quality, etc)"
+                        multiline
+                        value={imag.description}
+                        onChange={onChange}
+                        name="description"
+                      />
+                      </div>
+                  </Box> */}
                 <div>
                   <textarea
                     rows={5}
@@ -118,11 +158,29 @@ const Images = () => {
                     <option>Three</option>
                     <option>Four</option>
                   </select>
+                  {/* <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">SELECT NGO...</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        name="text"
+                        value={imag.text}
+                        label="Select NGO..."
+                        onChange={onChange}
+                      >
+                        <MenuItem value={"One"}>One</MenuItem>
+                        <MenuItem value={"Two"}>Two</MenuItem>
+                        <MenuItem value={"Three"}>Three</MenuItem>
+                        <MenuItem value={"Four"}>Four</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box> */}
                 </div>
                 <button
                   onClick={handleSubmit}
                   disabled={
-                    imag.title.length < 4 || imag.description.length < 10 || imag.text == "Select NGO..."
+                    imag.title.length < 4 || imag.description.length < 10 || imag.text == ""
                   }
                   className={`${styles.button} btn btn-warning my-3`}
                   type="submit"
@@ -139,3 +197,6 @@ const Images = () => {
 };
 
 export default Images;
+
+
+
