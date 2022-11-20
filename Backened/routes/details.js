025eb5cf6cbeca4f94463cff37b0deda610bd123
ayushjain27 <ayushjain201574@gmail.com>
2022-11-20 16:28:51 +0530

    Routes Updated

--- Backened/routes/details.js
+++ Backened/routes/details.js
@@ -16,7 +16,31 @@ router.get('/fetchalldetails', fetchuser, async (req, res) => {
 })
 
 // ROUTE 2:  Add a new Detail using: POST "/api/details/adddetail". login required
-router.post('/adddetail', fetchuser, [
+router.post('/adddetail/clothes', fetchuser, [
+    body('title', 'Title must be atleast 5 characters').isLength({ min: 5 }),
+    body('description', 'Description must be atleast 10 characters').isLength({ min: 10 }),
+], async (req, res) => {
+    try {
+        const { title, description, ngo_link, image } = req.body;
+        // if there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const detail = new Detail({
+            title, description, ngo_link, image, user: req.user.id
+        })
+        const savedDetail = await detail.save();
+        res.json(savedDetail);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+// ROUTE 3:  Add a new Detail using: POST "/api/details/adddetail". login required
+router.post('/adddetail/shoes', fetchuser, [
     body('title', 'Title must be atleast 5 characters').isLength({ min: 5 }),
     body('description', 'Description must be atleast 10 characters').isLength({ min: 10 }),
 ], async (req, res) => {
