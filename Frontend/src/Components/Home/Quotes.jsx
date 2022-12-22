f16c84b317968aa235aa52d6fdd4e02d1f50c8a4
kushagra sharma <2001kushagrasharma@gmail.com>
2022-12-22 11:06:51 +0530

    Updated

--- Frontend/src/Components/Home/Quotes.jsx
+++ Frontend/src/Components/Home/Quotes.jsx
@@ -11,7 +11,7 @@ const Quotes = () => {
               <p className={`${styles.text} fs-4 fw-bold text-center`}>Life gives you 100 reasons to smile. Be a reason for someone</p>
             </div>
         </div>
-        <div className={`${styles.JoinContainer} d-flex justify-content-center align-items-center w-100`}>
+        <div className={`${styles.JoinContainer} d-flex justify-content-center align-items-center w-100 p-4`}>
             <div className={styles.Join}>Join Us/Register</div>
         </div>
     </>
